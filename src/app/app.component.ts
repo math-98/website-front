6abba6cf89bf6e3ffec08317b6617e7f40da19eb
Mathieu SERVIERE <mathieu.serviere@oviance.com>
2020-08-14 10:20:12 +0200

    Setup routing
     - Add SeoService
     - Edit AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { SeoService } from './services/seo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'math98-v3-front';
+export class AppComponent implements OnInit {
+  public currentDate = new Date();
+  private router: Router;
+  private activatedRoute: ActivatedRoute;
+  private seoService: SeoService;
+
+  constructor(
+    router: Router,
+    activatedRoute: ActivatedRoute,
+    seoService: SeoService
+  ) {
+    this.router = router;
+    this.activatedRoute = activatedRoute;
+    this.seoService = seoService;
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        map(() => this.activatedRoute),
+        map((route: ActivatedRoute) => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter((route: ActivatedRoute) => route.outlet === 'primary'),
+        mergeMap((route: ActivatedRoute) => route.data)
+      )
+      .subscribe((event) => {
+        this.seoService.reset();
+
+        this.seoService.setPageTitle(event.title);
+        if (event.description) {
+          this.seoService.setDescription(event.description);
+        }
+        this.seoService.setTag('og:url', this.router.url);
+
+        if (event.social_image) {
+          this.seoService.setSocialImages(event.social_image);
+        }
+        if (event.social_image_alt) {
+          this.seoService.setTag('og:image:alt', event.social_image_alt);
+        }
+      });
+  }
 }
