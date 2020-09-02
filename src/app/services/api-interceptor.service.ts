e42a455818fc74874cd971094dfbb26626cee3e2
Mathieu SERVIERE <mathieu@math98.fr>
2020-09-02 09:26:54 +0200

    Setup API
     - Create ApiInterceptor service
     - Add ApiInterceptor to AppModule

---
+++ src/app/services/api-interceptor.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiInterceptorService implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const apiReq = req.clone({ url: environment.apiBase + '/' + req.url });
+
+    return next.handle(apiReq);
+  }
+}
