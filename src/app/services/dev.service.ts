3cddb09ef88a28dc8e045ec19e7831de926e9043
Mathieu SERVIERE <mathieu@math98.fr>
2020-09-15 19:54:51 +0200

    Edit home-skills component
     - Add dev service file
     - Add dev-language model
     - Edit home-skills.component.ts to make API calls
     - Edit home-skills.component.html to display languages or error
     - Edit home-skills.component.scss

---
+++ src/app/services/dev.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DevLanguage } from '../models/dev-language';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DevService {
+  private http: HttpClient;
+  private languages: object;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  public async listLanguages(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.http.get('api/v1/dev-languages/list').subscribe(
+        (data) => {
+          this.languages = {};
+
+          Object.entries(data).forEach(([key, languages]: [string, object[]]) => {
+            const category = [];
+            languages.forEach((language) => {
+              category.push(new DevLanguage(language));
+            });
+
+            this.languages[key] = category;
+          });
+
+          resolve(this.languages);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+    });
+  }
+}
--- src/app/views/home/home-skills/home-skills.component.ts
+++ src/app/views/home/home-skills/home-skills.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DevService } from '../../../services/dev.service';
+import { DevLanguage } from '../../../models/dev-language';
 
 @Component({
   selector: 'app-home-skills',
   templateUrl: './home-skills.component.html',
   styleUrls: ['./home-skills.component.scss'],
 })
-export class HomeSkillsComponent {}
+export class HomeSkillsComponent implements OnInit {
+  private devService: DevService;
+  public languages: object;
+
+  constructor(devService: DevService) {
+    this.devService = devService;
+  }
+
+  ngOnInit(): void {
+    this.languages = undefined;
+    this.devService
+      .listLanguages()
+      .then((languages) => {
+        this.languages = languages;
+
+        if (languages.length === 0) {
+          console.error('No dev language returned by API!');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.languages = [];
+      });
+  }
+}
