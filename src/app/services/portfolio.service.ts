f2df4f2e2b1d15e4743e5adc471549aaee466e73
Mathieu SERVIERE <mathieu@math98.fr>
2020-09-19 18:42:32 +0200

    Post view
     - Add portfolio-post component files
     - Add portfolio-post component to app.module.ts
     - Add post view route
     - Edit post url in home-portfolio component view
     - Add get method to portfolio service
     - Add languages list to portfolio-post model
     - Edit custom.scss

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -31,4 +31,31 @@ export class PortfolioService {
       );
     });
   }
+
+  public async get(slug): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const index = this.posts.findIndex((elm) => elm.slug === slug);
+      if (index >= 0) {
+        if (!this.posts[index].isMinified()) {
+          resolve(this.posts[index]);
+        }
+      }
+
+      this.http.get('api/v1/portfolio/get/' + slug).subscribe(
+        (data: object) => {
+          const post = new PortfolioPost(data);
+          if (index === undefined) {
+            this.posts.push(post);
+          } else {
+            this.posts[index] = post;
+          }
+
+          resolve(post);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+    });
+  }
 }
