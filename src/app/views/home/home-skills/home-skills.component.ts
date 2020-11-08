3b435a569b3e1c5abb2c03d564c8f35b27700a95
Mathieu SERVIERE <mathieu@math98.fr>
2020-11-09 00:08:44 +0100

    Home view
     - Add title component
     - Add home view
     - Add home-portfolio component
     - Add home-skills component
     - Add home route
     - Add load-awesome to NPM
     - Import load-awesome animations in styles.scss
     - Add ngx-spinner to NPM

---
+++ src/app/views/home/home-skills/home-skills.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-skills',
+  templateUrl: './home-skills.component.html',
+  styleUrls: ['./home-skills.component.scss'],
+})
+export class HomeSkillsComponent {}
