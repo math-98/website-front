80208e7c9651efdfd5837afb79f11bb5b7a44ab3
Mathieu SERVIERE <mathieu@math98.fr>
2020-11-22 21:00:57 +0100

    Edit loader
     - Remove load-awesome files
     - Remove usages of load-awesome
     - Edit loader component
     - Edit usages of loader component

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { LOADERS } from '../../lib/ngx-spinner.enum';
-import { BooleanInput } from 'ng-boolean-input';
 
 @Component({
   selector: 'app-loader',
@@ -8,15 +6,6 @@ import { BooleanInput } from 'ng-boolean-input';
   styleUrls: ['./loader.component.scss'],
 })
 export class LoaderComponent {
-  @Input() type;
-
-  @BooleanInput @Input() light;
-
-  get iterable(): Array<number> {
-    const arr = [];
-    for (let i = 0; i < LOADERS[this.type]; i++) {
-      arr.push(i);
-    }
-    return arr;
-  }
+  @Input() color = 'dark';
+  @Input() type = 'border';
 }
