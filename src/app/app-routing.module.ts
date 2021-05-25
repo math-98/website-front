c1ca14eebe787c4330d38cd2fceabf46f2c3e80d
Mathieu SERVIERE <mathieu@math98.fr>
2021-05-25 22:42:21 +0200

    Encrypt - Create route & view
     - Create component
     - Add route to app-routing.module.ts
     - Add route to sitemap.xml
     - Add component to app.module.ts
     - Add NPM dependency
     - Edit app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { PortfolioPostComponent } from './views/portfolio-post/portfolio-post.component';
+import { PasswordsComponent } from './views/passwords/passwords.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,14 @@ const routes: Routes = [
     component: HomeComponent,
     data: {
       title: 'Accueil',
+      description: '',
+    },
+  },
+  {
+    path: 'passwords',
+    component: PasswordsComponent,
+    data: {
+      title: 'Chiffrer un mot de passe',
       description: '',
     },
   },
